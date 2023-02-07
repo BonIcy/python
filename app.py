f97939b48a835066082c7337cb09a54dc4a01058
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-06 22:17:42 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+""" codigo python XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD """
+nombre = "YisusChrist"
+edad = 81
+estadoCivil = "viudo"
+hotel = "trivago"
+fechaHistorica = "11/09/2001"
+
+gustos = {"mujeres", "que me quieran (opcional)", "mayores", "mas mujeres", "con gatitos", "que hablen hebreo, latin y catalán"}
+
+noVarian = ["Lugar de nacimiento (Colombia)", "Presidente de Venezuela", ""]
+
+estadoActual {
+    Vivo: False
+    Feliz : True
+    Con sueño: True
+    Alucin: True   
+    Con ets
+        Sifilis: True
+        Sida: False 
+}
